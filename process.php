9769781652d8ef91d79d317a876d0bd10fa00b0f
nouailmed <123259509+nouailmed@users.noreply.github.com>
2023-06-19 20:25:07 +0100

    Update process.php

--- process.php
+++ process.php
@@ -1,16 +1,24 @@
 <?php
 
-    $to = "";
+    $to = "nouailmed03@gmail.com";
     $from = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
     $name = $_REQUEST['name'];
+    $Service = $_REQUEST['Service'];
+    $Secteur = $_REQUEST['Secteur'];
+    $number = $_REQUEST['number'];
+
     $headers = "From: $from";
+  
 
     $fields = array();
     $fields{"name"} = "name";
     $fields{"email"} = "email";
     $fields{"subject"} = "subject";
     $fields{"message"} = "message";
+    $fields{"Service"} = "Service";    
+    $fields{"Secteur"} = "Secteur";
+    $fields{"number"} = "number";
 
     $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
 
